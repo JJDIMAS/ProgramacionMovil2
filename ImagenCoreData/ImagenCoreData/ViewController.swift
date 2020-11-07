1cad0e78a87f95741910a2588959d4ad2d3db287
Jennifer Dimas <jennifer.correctivo@gmail.com>
2020-11-07 01:34:44 -0600

    Core data
    General design

---
+++ ImagenCoreData/ImagenCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagenCoreData
+//
+//  Created by Mac18 on 06/11/20.
+//  Copyright © 2020 itm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
