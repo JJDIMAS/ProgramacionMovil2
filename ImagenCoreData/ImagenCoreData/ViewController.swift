dcfe73f53a63f7ebda337369b543a7d436d10a07
Jennifer Dimas <jennifer.correctivo@gmail.com>
2020-11-08 16:09:17 -0600

    PickController
    PickController

--- ImagenCoreData/ImagenCoreData/ViewController.swift
+++ ImagenCoreData/ImagenCoreData/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var ImagenPerfil: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func elegirFoto(_ sender: UIButton) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagen = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage
+        ImagenPerfil.image = imagen
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func GuardarImagen(_ sender: UIButton) {
+        
+    }
 }
-
