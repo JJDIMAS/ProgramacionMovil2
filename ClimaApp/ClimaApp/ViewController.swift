468b04c0e0bea6cc11679bf9083caeddd5dd7619
Jennifer Dimas <jennifer.correctivo@gmail.com>
2020-11-22 13:44:25 -0600

    General design
    Icon, launchscreen and storyboard set

---
+++ ClimaApp/ClimaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimaApp
+//
+//  Created by Mac18 on 22/11/20.
+//  Copyright © 2020 itm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
